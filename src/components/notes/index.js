e137f1b53acb151eed4043ba8d6a721103132b7c
Luís Almeida <luisalmeida@MacBook-Pro.Home>
2019-10-25 23:22:25 +0100

    Reading data from MongoDB Atlas and showing notes on the component.

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
+import Modal from 'react-awesome-modal';
+
+import axios from 'axios';
+
+
+
 import './styles.css';
 
 export default class Notes extends Component {
 
+    state = {
+            notes: [],
+            visible: false
+    }
+
+    componentDidMount() {
+
+        this.loadNotes();
+       
+
+    }
+
+    loadNotes() {
+        axios.get('http://localhost:3002/api/notes')
+        .then(res => {
+            const notes = res.data;
+            this.setState({ notes });
+        })
+    }
+    
+    // MODAL WINDOW ACTIONS
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+ 
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
 
     render() {
         return (
@@ -12,6 +50,8 @@ export default class Notes extends Component {
                     <div className="main-header-notes">
 
                         <div className="categories-bar">
+                            
+                            
                             <ul>
                                 <li className="selected">All</li>
                                 <li>Business</li>
@@ -21,27 +61,42 @@ export default class Notes extends Component {
                         </div>
                     
                         <div className="qa-new-note">
-                            <small>+ Add new Note</small>
+                            
+                            <small>
+                                <span onClick={() => this.openModal() }>
+                                    + Add new Note
+                                </span>
+                            </small>
+                            {/* <input type="button" value="Open" onClick={() => this.openModal()} /> */}
+
+                           
+
+
                         </div>
                     </div>
                     <div className="notes-content">
-                            <article>
-                                <div className="date">
-                                    20 June, 2019
-                                </div>
-                                <div className="title">
-                                    this is my first title.
-                                    this is my first title.
-                                </div>
-                                <p>
-                                    Lorem ipsum dolor it cum to me and let's see what happen in the future
-                                    Lorem ipsum dolor it cum to me and let's see what happen in the future
-                                    Lorem ipsum dolor it cum to me and let's see what happen in the future
-                                    Lorem ipsum dolor it cum to me and let's see what  Lorem ipsum dolor it cum to me and let's see what happen in the future
-                                    Lorem ipsum dolor it cum to me and let's see what happen in the future
-                                </p>
-                                <div className="footer">this is my footer</div>
-                            </article>
+
+                    {/* {this.state.notes.map(note => <li>{ note.title }</li>)} */}
+
+                    {this.state.notes.map((note) => (
+                        <article>
+                        <div className="date">
+                            {note.createdAt}
+                        </div>
+                        <div className="title">
+                            { note.title }
+                            
+                        </div>
+                        <p>
+                            { note.body }
+                            
+                        </p>
+                        <div className="footer">note.categories</div>
+                        </article>
+
+                ))}
+
+                          
                             <article>
                                 <div className="date">
                                     20 June, 2019
@@ -60,7 +115,14 @@ export default class Notes extends Component {
                         </div>
                     </div>
                     
-                   
+                    {/* MODAL WINDOW */}
+                    <Modal visible={this.state.visible} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+                                <div className="modal-class">
+                                    <h1>Title</h1>
+                                    <p>Some Contents</p>
+                                    <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                                </div>
+                    </Modal>
                 
             </div>
         );
